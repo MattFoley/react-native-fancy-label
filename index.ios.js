9b024bf14130b3faf9a9a40d9eca30c13d229957
Greg Herlihy <gherlihy@skillz.com>
2016-02-08 08:56:37 -0800

    use lodash's keys() method since the normal .keys property doesn't return function names in the object.

--- index.ios.js
+++ index.ios.js
@@ -68,8 +68,8 @@ class FancyLabel extends React.Component {
   render() {
       var styleProps = typeof this.props.style == 'number' ? StyleSheetRegistry.getStyleByID(this.props.style) :
                                                              this.props.style;
-      var viewProps = { ...this.props, style: {..._.omit(styleProps, FancyLabelOwnPropTypes.keys)}};
-      var labelProps = _.pick(styleProps, FancyLabelOwnPropTypes.keys);
+      var viewProps = { ...this.props, style: {..._.omit(styleProps, ._keys(FancyLabelOwnPropTypes))}};
+      var labelProps = _.pick(styleProps, _.keys(FancyLabelOwnPropTypes));
       var textProps = _.pick(styleProps, ['fontSize', 'fontFamily', 'letterSpacing']);
 
       if (processColor) {
